dab998087f8a228a6f558c16dd3c54a61b8120d9
nazmul ahsan <tarekahsan709@gmail.com>
2021-01-06 18:27:22 +0600

    Refactor routes

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
